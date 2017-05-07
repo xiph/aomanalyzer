0200ac8a88b2cff0dd3cad88ad2b8f8a3eccb58b
Michael Bebenita <mbebenita@gmail.com>
2017-05-07 11:00:12 -0700

    Clean up imports, and remove dead code.

--- src/components/Download.tsx
+++ src/components/Download.tsx
@@ -1,20 +1,7 @@
 import * as React from "react";
 
-import Paper from 'material-ui/Paper';
-import Dialog from 'material-ui/Dialog';
-import { Toolbar, ToolbarGroup, ToolbarSeparator, ToolbarTitle } from 'material-ui/Toolbar';
-import IconButton from 'material-ui/IconButton';
-import FontIcon from 'material-ui/FontIcon';
-import Checkbox from 'material-ui/Checkbox';
-import { grey900, grey800, grey100, grey200, red100, red500, red600, red700, red800, red900, deepOrange500 } from 'material-ui/styles/colors';
-import { assert, clamp, downloadFile, Decoder, AnalyzerFrame, FrameImage } from "./analyzerTools";
-import LinearProgress from 'material-ui/LinearProgress';
+import { Decoder, AnalyzerFrame, downloadFile, FrameImage } from './analyzerTools';
 import CircularProgress from 'material-ui/CircularProgress';
-import RaisedButton from 'material-ui/RaisedButton';
-import FlatButton from 'material-ui/FlatButton';
-import { Table, TableBody, TableHeader, TableHeaderColumn, TableRow, TableRowColumn } from 'material-ui/Table';
-import Toggle from 'material-ui/Toggle';
-import TextField from 'material-ui/TextField';
 import { saveAs } from 'file-saver';
 
 interface DownloadComponentProps {
@@ -32,9 +19,6 @@ export class DownloadComponent extends React.Component<DownloadComponentProps, {
 
   decoder: Decoder;
   y4m: Blob;
-  playerInterval: number;
-  fetchPumpInterval: number;
-  drainFetchPumpInterval: number;
   wroteHeader: boolean = false;
 
   constructor() {
@@ -48,18 +32,6 @@ export class DownloadComponent extends React.Component<DownloadComponentProps, {
   frames: AnalyzerFrame[] = [];
   frameBuffer: AnalyzerFrame[] = [];
 
-  pauseIfPlaying() {
-    if (this.playerInterval) {
-      clearInterval(this.playerInterval);
-      this.playerInterval = 0;
-    }
-  }
-
-  /**
-   * Not the React way.
-   */
-  isComponentMounted: boolean = false;
-
   componentDidMount() {
     this.setState({ status: "Loading Decoder" } as any);
 
@@ -70,73 +42,50 @@ export class DownloadComponent extends React.Component<DownloadComponentProps, {
         decoder.setLayers(0);
         this.decoder = decoder;
         this.setState({ decoder } as any);
-        this.initialize();
+        this.dumpFrames();
       });
     });
-    this.isComponentMounted = true;
-  }
-
-  componentWillUnmount() {
-    this.pauseIfPlaying();
-    this.isComponentMounted = false;
-  }
-
-  forceUpdateIfMounted() {
-    if (this.isComponentMounted) {
-      this.forceUpdate();
-    } else {
-      console.warn("Shouldn't be updating anymore.");
-    }
   }
 
   dumpY4MFrame(image: FrameImage) {
     if (!this.wroteHeader) {
-      this.y4m = new Blob(["YUV4MPEG2 C420jpeg W"+image.Y.width+" H"+image.Y.height+"\n"]);
+      this.y4m = new Blob(["YUV4MPEG2 C420jpeg W" + image.Y.width + " H" + image.Y.height + "\n"]);
       this.wroteHeader = true;
     }
     this.y4m = new Blob([this.y4m, "FRAME\n"]);
     for (let plane of [image.Y, image.U, image.V]) {
       let plane_uint8 = new Uint8Array(plane.buffer);
-      let plane_export = new Uint8Array(plane.width*plane.height);
+      let plane_export = new Uint8Array(plane.width * plane.height);
       for (let y = 0; y < plane.height; y++) {
         for (let x = 0; x < plane.width; x++) {
-          plane_export[y*plane.width+x] = plane_uint8[y*plane.stride+x];
+          plane_export[y * plane.width + x] = plane_uint8[y * plane.stride + x];
         }
       }
-      this.y4m = new Blob([this.y4m,plane_export]);
+      this.y4m = new Blob([this.y4m, plane_export]);
     }
   }
 
   dumpFrames() {
     this.decoder.readFrame().then(frames => {
-      this.setState({status: "Decoding video"} as any);
+      this.setState({ status: "Decoding video" } as any);
       frames.forEach(frame => {
         let image = frame.frameImage;
         this.dumpY4MFrame(image);
       });
-        this.dumpFrames();
+      this.dumpFrames();
     }, () => {
-      this.setState({status: "Complete!"} as any);
-      saveAs(this.y4m,this.props.filename,true);
+      this.setState({ status: "Complete!" } as any);
+      saveAs(this.y4m, this.props.filename, true);
     });
   }
 
-  initialize() {
-    this.dumpFrames();
-  }
-
-  ignoreNextScrollEvent = false;
   render() {
-    let valueStyle = { textAlign: "right", fontSize: "12px" };
-
-    let allStats, lastStats, benchStats;
     if (this.state.status != "Complete!") {
       return <div className="playerCenterContainer">
-  <div className="playerCenterContent">
-
-      <CircularProgress size={40} thickness={7} /><br /><br />
-    {this.state.status}
-  </div>
+        <div className="playerCenterContent">
+          <CircularProgress size={40} thickness={7} /><br /><br />
+          {this.state.status}
+        </div>
       </div>
     } else {
       return <div className="playerCenterContainer">
@@ -146,5 +95,4 @@ export class DownloadComponent extends React.Component<DownloadComponentProps, {
       </div>
     }
   }
-
 }
